b3860106839b87161c3c77a12438bc98731424fa
Oleg Semchenko <ogresemchenko326@gmail.com>
2025-02-15 15:02:11 +0700

    Add GetActiveTokenByUserID for logic

--- internal/domain/repository/auth_token_repository.go
+++ internal/domain/repository/auth_token_repository.go
@@ -1,11 +1,13 @@
 package repository
 
 import (
+	"github.com/google/uuid"
 	"merch-service/internal/domain/model"
 )
 
 type AuthTokenRepository interface {
 	Create(token *model.AuthToken) error
+	GetActiveTokenByUserID(userID uuid.UUID) (*model.AuthToken, error)
 	GetByToken(token string) (*model.AuthToken, error)
 	Delete(token string) error
 }
