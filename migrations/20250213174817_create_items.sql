9bf2a7b2660cf146c1835586468775b3bbbc84c6
Oleg Semchenko <ogresemchenko326@gmail.com>
2025-02-15 15:02:41 +0700

    Add default items initializing

--- migrations/20250213174817_create_items.sql
+++ migrations/20250213174817_create_items.sql
@@ -5,6 +5,19 @@ CREATE TABLE IF NOT EXISTS items (
     title TEXT UNIQUE NOT NULL,
     price INT NOT NULL CHECK ( price > 0 )
 );
+
+-- Добавление начальных записей
+INSERT INTO items (title, price) VALUES
+                                     ('t-shirt', 80),
+                                     ('cup', 20),
+                                     ('book', 50),
+                                     ('pen', 10),
+                                     ('powerbank', 200),
+                                     ('hoody', 300),
+                                     ('umbrella', 200),
+                                     ('socks', 10),
+                                     ('wallet', 50),
+                                     ('pink-hoody', 500);
 -- +goose StatementEnd
 
 -- +goose Down
