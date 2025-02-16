b2ebcc6dfae5a3183aac26299050ede7852ef59b
Oleg Semchenko <ogresemchenko326@gmail.com>
2025-02-16 21:03:56 +0700

    Add default users

--- migrations/20250213174800_create_users.sql
+++ migrations/20250213174800_create_users.sql
@@ -8,6 +8,10 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
+
+INSERT INTO users (id, username, password) VALUES
+                                     ('000d41d4-dd00-4ed4-8d14-5483e3147b31', 'oleg', 'passwd123'),
+                                     ('e150ea00-ad91-4459-852e-06f05d60c77b', 'vlad', 'passwd123');
 -- +goose StatementEnd
 
 -- +goose Down
