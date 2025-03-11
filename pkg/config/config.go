682432a72c2d643504154e4f4c9585081d62b5fc
Oleg Semchenko <ogresemchenko326@gmail.com>
2025-03-11 17:05:02 +0700

    Move config to pkg

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -26,7 +26,7 @@ func Get() Config {
 	once.Do(func() {
 		loader := aconfig.LoaderFor(&cfg, aconfig.Config{
 			EnvPrefix: "NFB",
-			Files:     []string{"./config.hcl", "./config.local.hcl"},
+			Files:     []string{"../config.hcl", "../config.local.hcl"},
 			FileDecoders: map[string]aconfig.FileDecoder{
 				".hcl": aconfighcl.New(),
 			},
